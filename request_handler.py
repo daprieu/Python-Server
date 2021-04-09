944c8c71f2c0ab5d911b4dcde26b38051b619b92
daprieu <daprieu@gmail.com>
2021-04-09 11:46:38 -0500

    delete employee

--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
-from employees import get_all_employees, get_single_employee, create_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer
 
 # Here's a class. It inherits from another class.
@@ -130,6 +130,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             delete_animal(id)
         if resource == "locations":
             delete_location(id)
+        if resource == "employees":
+            delete_employee(id)
 
         # Encode the new animal and send in response
         self.wfile.write("".encode())
