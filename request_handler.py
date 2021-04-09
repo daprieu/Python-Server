cf70c24b6ce58df982fa03e2ba7ccd7dfff084ca
daprieu <daprieu@gmail.com>
2021-04-09 11:42:39 -0500

    delete locations

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
-from locations import get_all_locations, get_single_location, create_location
+from locations import get_all_locations, get_single_location, create_location, delete_location
 from employees import get_all_employees, get_single_employee, create_employee
 from customers import get_all_customers, get_single_customer, create_customer
 
@@ -128,6 +128,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Delete a single animal from the list
         if resource == "animals":
             delete_animal(id)
+        if resource == "locations":
+            delete_location(id)
 
         # Encode the new animal and send in response
         self.wfile.write("".encode())
