0b653d0aa931b3367b581587fb5e935c1d95fb8f
daprieu <daprieu@gmail.com>
2021-04-12 09:31:15 -0500

    puts for animals and customers

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
-from customers import get_all_customers, get_single_customer, create_customer, delete_customer
+from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -56,29 +56,29 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         if resource == "animals":
             if id is not None:
-                response = f"{get_single_animal(id)}"
+                response = get_single_animal(id)
             else:
-                response = f"{get_all_animals()}"
+                response = get_all_animals()
 
         if resource == "locations":
             if id is not None:
-                response = f"{get_single_location(id)}"
+                response = get_single_location(id)
             else:
-                response = f"{get_all_locations()}"
+                response = get_all_locations()
 
         if resource == "employees":
             if id is not None:
-                response = f"{get_single_employee(id)}"
+                response = get_single_employee(id)
             else:
-                response = f"{get_all_employees()}"
+                response = get_all_employees()
         
         if resource == "customers":
             if id is not None:
-                response = f"{get_single_customer(id)}"
+                response = get_single_customer(id)
             else:
-                response = f"{get_all_customers()}"
+                response = get_all_customers()
 
-        self.wfile.write(response.encode())
+        self.wfile.write(json.dumps(response).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
@@ -116,7 +116,22 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
     def do_PUT(self):
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+        if resource == "customers":
+            update_customer(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
         # Set a 204 response code
